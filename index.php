2696b1f97a703a84fb81b077ac0a70cac0e2fdc7
JGB173 <78768399+JGB173@users.noreply.github.com>
2022-05-18 14:57:47 +1200

    added social media icons and positioned them. I also got images for the green tent. Started working on the header background.

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The name or airbnb is an airbnb in Matakana where you can stay">
+    <meta name="keywords" content="Airbnb, bnb, Matakana, Omaha, The Green Tent, Green Tent,">
+    <meta name="author" content="Josh Burgess">
+
+    <title>The Green Tent</title>
+    <!-- link to css -->
+    <link href="css/the-green-tent_style.css" rel="stylesheet">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@700&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <!-- link to social media icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+
 <body>
+    <div class="container">
 
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
+        <header>Header</header>
+        <main>Main</main>
+        <footer>
+            <div class="social">
+              <h1>Follow Us</h1>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+        </footer>
+        
+    </div> <!-- container -->
 
 </body>
 </html>
