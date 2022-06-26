c7fb4d25ca04f6320aa902ae1a02474cf51aa7e0
JGB173 <78768399+JGB173@users.noreply.github.com>
2022-06-27 09:49:34 +1200

    I have finished the gallery and have started creating the contact form on the contact page

--- contact.php
+++ contact.php
@@ -14,8 +14,85 @@
 
             <?php include 'mobile-nav.php'; ?>
 
-        <main>
-            <h1>Contact Us</h1>
+        <main class="contact-page">
+            <div class="contact-us">
+                <div class="title">
+                    <h2>Get in Touch</h2>
+                </div>
+                <div class="box">
+                    <!-- Form -->
+                    <div class="contact form">
+                        <h3>Send a Message</h3>
+                        <form>
+                            <div class="formBox">
+                                <div class="row50">
+                                    <div class="inputBox">
+                                        <span>First Name</span>
+                                        <input type="text" placeholder="John">
+                                    </div>
+                                    <div class="inputBox">
+                                        <span>Last Name</span>
+                                        <input type="text" placeholder="Doe">
+                                    </div>
+                                </div>
+
+                                <div class="row50">
+                                    <div class="inputBox">
+                                        <span>Email</span>
+                                        <input type="text" placeholder="johndoe@email.com">
+                                    </div>
+                                    <div class="inputBox">
+                                        <span>Mobile</span>
+                                        <input type="text" placeholder="+91 987 654 3210">
+                                    </div>
+                                </div>
+
+                                <div class="row100">
+                                    <div class="inputBox">
+                                        <span>Message</span>
+                                        <textarea placeholder="Write your message here..."></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row100">
+                                    <div class="inputBox">
+                                        <input type="submit" value="Send">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- Info Box -->
+                    <div class="contact info">
+                        <h3>Contact Info</h3>
+                        <div class="infoBox">
+                            <div>
+                                <span><ion-icon name="location"></ion-icon></span>
+                                <p>Tongue Farm Road Matakana 0985, Auckland <br>New Zealand</p>
+                            </div>
+                            <div>
+                                <span><ion-icon name="mail"></ion-icon></span>
+                                <a href="#mailto:stay@thegreentent.co.nz">stay@thegreentent.co.nz</a>
+                            </div>
+                            <div>
+                                <span><ion-icon name="call"></ion-icon></span>
+                                <a href="#tel:+64(0)212162320">+64 (0) 21 2162320</a>
+                            </div>
+                            <!-- social media icons -->
+                            <ul class="sci">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Map -->
+                    <div class="contact map"></div>
+                </div>
+            </div>
         </main>
 
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <?php include 'bottom.php';?>
--- gallery.php
+++ gallery.php
@@ -16,12 +16,18 @@
         <main>
             <div class="gallery-page-main">
                 <img class="gallery-img" id="gallery-1" alt="Image of inside" src="Images/the-green-tent-gallery-lounge-v1.jpg">
-                <img class="gallery-img" id="gallery-2" alt="Image of deck" src="Images/the-green-tent-gallery-bed-v1.jpg">
-                <img class="gallery-img" id="gallery-3" alt="Image of deck" src="Images/the-green-tent-gallery-couch-v1.jpg">
-                <img class="gallery-img" id="gallery-4" alt="Image of kitchen" src="Images/the-green-tent-gallery-bath-v1.jpg">
-                <img class="gallery-img" id="gallery-5" alt="Image of kitchen" src="Images/the-green-tent-gallery-outside-deck-v1.jpg">
-                <img class="gallery-img" id="gallery-6" alt="Image of kitchen" src="Images/the-green-tent-gallery-kitchen-v1.jpg">
-                <img class="gallery-img" id="gallery-7" alt="Image of kitchen" src="Images/the-green-tent-gallery-outside-seating-v1.jpg">
+                <img class="gallery-img" id="gallery-2" alt="Image of bed side" src="Images/the-green-tent-gallery-bed-v1.jpg">
+                <img class="gallery-img" id="gallery-3" alt="Image of bed" src="Images/the-green-tent-gallery-bed2-v1.jpg">
+                <img class="gallery-img" id="gallery-4" alt="Image of couch" src="Images/the-green-tent-gallery-couch-v1.jpg">
+                <img class="gallery-img" id="gallery-5" alt="Image of plant closeup" src="Images/the-green-tent-gallery-plant-closeup-v1.jpg">
+                <img class="gallery-img" id="gallery-6" alt="Image of doorway" src="Images/the-green-tent-gallery-doorway-v1.jpg">
+                <img class="gallery-img" id="gallery-7" alt="Image of walk way between kitchen and yurt" src="Images/the-green-tent-gallery-outside-deck-v1.jpg">
+                <img class="gallery-img" id="gallery-8" alt="Image of bath closeup" src="Images/the-green-tent-gallery-bath-v1.jpg">
+                <img class="gallery-img" id="gallery-9" alt="Image of kitchen" src="Images/the-green-tent-gallery-kitchen-v1.jpg">
+                <img class="gallery-img" id="gallery-10" alt="Image of kitchen closeup" src="Images/the-green-tent-gallery-kitchen-closeup-v1.jpg">
+                <img class="gallery-img" id="gallery-11" alt="Image of outside seating" src="Images/the-green-tent-gallery-outside-seating-v1.jpg">
+                <img class="gallery-img" id="gallery-12" alt="Image of table" src="Images/the-green-tent-gallery-table-closeup-v1.jpg">
+                <img class="gallery-img" id="gallery-13" alt="Image of yurt from far away" src="Images/the-green-tent-longshot-v1.jpg">
             </div>
         </main>
 
