fa2a00908d96275febe10e524dbf2e2cd3d79f0f
JGB173 <78768399+JGB173@users.noreply.github.com>
2022-06-28 10:25:58 +1200

    Improved galleries by making all images sizes equal

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
         <main>
             <h1>Gallery</h1>
             <div class="home-gallery">
-                <img class="home-gallery-img" id="home-gallery-1" alt="Image of inside" src="Images/the-green-tent-home-v2.jpg">
-                <img class="home-gallery-img" id="home-gallery-2" alt="Image of deck" src="Images/the-green-tent-deck-v1.jpg">
-                <img class="home-gallery-img" id="home-gallery-3" alt="Image of deck" src="Images/the-green-tent-outside-v1.jpg">
-                <img class="home-gallery-img" id="home-gallery-4" alt="Image of kitchen" src="Images/the-green-tent-kitchen-v1.jpg">
+                <img class="home-gallery-img" id="home-gallery-1" alt="Image of inside" src="Images/the-green-tent-gallery-lounge-v1.jpg">
+                <img class="home-gallery-img" id="home-gallery-2" alt="Image of deck" src="Images/the-green-tent-gallery-outside-seating-v1.jpg">
+                <img class="home-gallery-img" id="home-gallery-3" alt="Image of deck" src="Images/the-green-tent-gallery-outside-deck-v1.jpg">
+                <img class="home-gallery-img" id="home-gallery-4" alt="Image of kitchen" src="Images/the-green-tent-gallery-kitchen-v1.jpg">
             </div>
         </main>
 
