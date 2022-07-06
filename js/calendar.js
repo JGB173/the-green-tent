90d3336b6277b031f45e38f2fca97789a3783d47
JGB173 <78768399+JGB173@users.noreply.github.com>
2022-07-06 12:14:33 +1200

    calendar js nearly complete

--- js/calendar.js
+++ js/calendar.js
@@ -1,8 +1,16 @@
 const date = new Date();
 
+date.setDate(1);
+
+console.log(date.getDate());
+
 const monthDays = document.querySelector('.days')
 
-const lastDay = new Date(date.getFullYear())
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay()
 
 const months = [
     "January",
@@ -25,7 +33,11 @@ document.querySelector(".date p").innerHTML = date.toDateString();
 
 let days = "";
 
-for(let i = 1; i <= 31; i++) {
+for(let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+}
+
+for(let i = 1; i <= lastDay; i++) {
     days += `<div>${i}</div>`;
     // x += 10    x = x + 10
     monthDays.innerHTML = days;
