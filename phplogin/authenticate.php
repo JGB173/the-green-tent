e74364b41455c8074aea1344d194885f1c80b0be
JGB173 <78768399+JGB173@users.noreply.github.com>
2022-08-11 14:29:38 +1200

    Created login page

--- phplogin/authenticate.php
+++ phplogin/authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'thegreentent';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
@@ -17,7 +17,6 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
-
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
@@ -38,7 +37,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
-            echo 'Welcome ' . $_SESSION['name'] . '!';
+            header('Location: home.php');
         } else {
             // Incorrect password
             echo 'Incorrect username and/or password!';
@@ -48,7 +47,8 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
         echo 'Incorrect username and/or password!';
     }
 
+}
+
 
 	$stmt->close();
-}
 ?>
